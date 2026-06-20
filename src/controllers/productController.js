7ca92d1f174d71cda0879ca2592050cb09f2e973
Navaneeth PV <navaneethpv550@gmail.com>
2026-06-20 18:37:38 +0530

    add update and delete functionality for products with routes

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -74,3 +74,60 @@ exports.getProduct = async (req, res) => {
   }
 };
 
+// Update a product
+
+exports.updateProduct = async (req, res) => {
+  try {
+    const product =
+      await Product.findByIdAndUpdate(
+        req.params.id,
+        req.body,
+        {
+          new: true,
+          runValidators: true,
+        }
+      );
+
+    if (!product) {
+      return res.status(404).json({
+        success: false,
+        message: "Product not found",
+      });
+    }
+
+    res.status(200).json({
+      success: true,
+      product,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
+};
+
+//Delete a product
+exports.deleteProduct = async (req, res) => {
+  try {
+    const product =
+      await Product.findById(req.params.id);
+
+    if (!product) {
+      return res.status(404).json({
+        success: false,
+        message: "Product not found",
+      });
+    }
+
+    await product.deleteOne();
+
+    res.status(200).json({
+      success: true,
+      message: "Product deleted",
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
+};
--- src/routes/productRoute.js
+++ src/routes/productRoute.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const authMiddleware = require('../middleware/authMiddleware');
-const {createProduct, getProducts, getProduct} = require('../controllers/productController');
+const {createProduct, getProducts, getProduct, updateProduct, deleteProduct} = require('../controllers/productController');
 
 // Create a new product
 router.post('/add', authMiddleware, createProduct);
 router.get('/', authMiddleware, getProducts);
 router.get('/:id', authMiddleware, getProduct);
-
+router.put('/:id', authMiddleware, updateProduct);
+router.delete('/:id', authMiddleware, deleteProduct);
 module.exports = router;
