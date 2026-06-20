d32bee7ead06f97e9b5548f686b4ff3fb29d918d
Navaneeth PV <navaneethpv550@gmail.com>
2026-06-20 12:10:31 +0530

    add getCategories function and update categoryRouter to include category listing route

--- src/controllers/categoryController.js
+++ src/controllers/categoryController.js
@@ -2,18 +2,46 @@ const Category = require("../models/Category");
 
 // Create a new category
 exports.createCategory = async (req, res) => {
-    try {
-        const { name } = req.body;
-        if(!name) {
-            return res.status(400).json({ success:false, message: "Category name is required" });
-        }
-        const existingCategory = await Category.findOne({name});
-        if(existingCategory) {
-            return res.status(400).json({ success:false, message: "Category already exists" });
-        }
-        const category = await Category.create({ name });
-        res.status(201).json({ success:true, message: `Category with name ${name} created` })
-    } catch (error) {
-        res.status(500).json({ success:false, message: "Internal server error" });
+  try {
+    const { name } = req.body;
+    if (!name) {
+      return res
+        .status(400)
+        .json({ success: false, message: "Category name is required" });
     }
+    const existingCategory = await Category.findOne({ name });
+    if (existingCategory) {
+      return res
+        .status(400)
+        .json({ success: false, message: "Category already exists" });
+    }
+    const category = await Category.create({ name });
+    res
+      .status(201)
+      .json({ success: true, message: `Category with name ${name} created` });
+  } catch (error) {
+    res
+      .status(500)
+      .json({
+        success: false,
+        message: "Internal server error while creating category",
+      });
+  }
+};
+
+//Get all categories
+exports.getCategories = async (req, res) => {
+  try {
+    const categories = await Category.find().sort({ createdAt: -1 });
+    res
+      .status(200)
+      .json({ success: true, categories, count: categories.length });
+  } catch (error) {
+    res
+      .status(500)
+      .json({
+        success: false,
+        message: "Internal server error while fetching categories",
+      });
+  }
 };
--- src/routes/categoryRouter.js
+++ src/routes/categoryRouter.js
@@ -2,8 +2,12 @@ const express = require("express");
 const router = express.Router();
 
 const authMiddleware = require("../middleware/authMiddleware");
-const { createCategory } = require("../controllers/categoryController");
-    
+const {
+  createCategory,
+  getCategories,
+} = require("../controllers/categoryController");
+
 router.post("/create", authMiddleware, createCategory);
+router.get("/list", authMiddleware, getCategories);
 
 module.exports = router;
