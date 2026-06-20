08e09d3df7a8bf58bbcbb406bb6b910082384ca9
Navaneeth PV <navaneethpv550@gmail.com>
2026-06-20 12:34:49 +0530

    add deleteCategory function and route for category deletion

--- src/controllers/categoryController.js
+++ src/controllers/categoryController.js
@@ -66,3 +66,23 @@ exports.updateCategory = async (req, res) => {
     });
   }
 };
+
+// Delete a category by ID
+exports.deleteCategory = async (req, res) => {
+    try {
+        const category = await Category.findById(req.params.id);
+        if (!category) {
+            return res.status(404).json({ success: false, message: "Category not found"});
+        }
+       await category.deleteOne();
+       res.status(200).json({ 
+        success: true,
+        message: `Category with name ${category.name} deleted successfully`
+       })
+    }catch (error) {
+        res.status(500).json({
+            success:false,
+            message: "Internal server error while deleting category"
+        })
+    }
+}
--- src/routes/categoryRouter.js
+++ src/routes/categoryRouter.js
@@ -6,10 +6,12 @@ const {
   createCategory,
   getCategories,
   updateCategory,
+  deleteCategory,
 } = require("../controllers/categoryController");
 
 router.post("/create", authMiddleware, createCategory);
 router.get("/list", authMiddleware, getCategories);
 router.put("/update/:id", authMiddleware, updateCategory);
+router.delete("/delete/:id", authMiddleware, deleteCategory);
 
 module.exports = router;
